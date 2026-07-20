8b607819de14752e662f9a679d14f3e477b83313
Arien Destriza <yindestriza@gmail.com>
2026-07-20 15:04:47 +0800

    Configure dynamic connection string in Global.asax

--- ASP Final Project/Global.asax.cs
+++ ASP Final Project/Global.asax.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -16,6 +16,47 @@ namespace ASP_Final_Project
             // Code that runs on application startup
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+        }
+
+        void Application_BeginRequest(object sender, EventArgs e)
+        {
+            try
+            {
+                var connSettings = System.Configuration.ConfigurationManager.ConnectionStrings["ArixBankCon"];
+                if (connSettings != null)
+                {
+                    string host = HttpContext.Current.Request.Url.Host.ToLower();
+                    string targetConnString = "";
+
+                    // If running locally, connect to the local MDF file (LocalDB)
+                    if (host.Contains("localhost") || host.Contains("127.0.0.1"))
+                    {
+                        targetConnString = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\ArixBankDB.mdf;Integrated Security=True";
+                    }
+                    else
+                    {
+                        // If running on the remote server (Somee), connect to Somee's SQL Server
+                        targetConnString = "workstation id=ArixBankDB.mssql.somee.com;packet size=4096;user id=yenyan_SQLLogin_1;pwd=almkn7wkga;data source=ArixBankDB.mssql.somee.com;persist security info=False;initial catalog=ArixBankDB;TrustServerCertificate=True";
+                    }
+
+                    // Apply the connection string dynamically using reflection
+                    if (connSettings.ConnectionString != targetConnString)
+                    {
+                        var readOnlyField = typeof(System.Configuration.ConfigurationElement)
+                            .GetField("_readOnly", System.Reflection.BindingFlags.Instance | System.Reflection.BindingFlags.NonPublic);
+                        
+                        if (readOnlyField != null)
+                        {
+                            readOnlyField.SetValue(connSettings, false);
+                            connSettings.ConnectionString = targetConnString;
+                        }
+                    }
+                }
+            }
+            catch (Exception)
+            {
+                // Silently ignore to prevent app crash if configuration is locked
+            }
         }
     }
 }
